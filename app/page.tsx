72fd32153b83e003d28b355b338099316a96d7fc
Robin de Niet <accounts@cobeas.nl>
2024-08-20 20:45:48 +0200

    env test

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
-"use client";
+// "use client";
 
-import { useRouter } from "next/navigation"
+// import { useRouter } from "next/navigation"
 
 export default function Home() {
-  const router = useRouter()
+  // const router = useRouter()
 
   return (
     <div>
       <h1>Home</h1>
-      <button onClick={() => router.push("/user/login")}>Login</button>
+      {/* <button onClick={() => router.push("/user/login")}>Login</button> */}
       <p>You are in the {process.env.NODE_ENV} environment</p>
       <p>The jwt issuer is {process.env.JWT_ISSUER}</p>
     </div>
